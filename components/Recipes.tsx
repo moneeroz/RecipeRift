e8d38bd581d09a14bb1745dda34c400ab885b2ef
Moneer <moneer.alromimah@gmail.com>
2023-12-19 02:16:59 -0700

    add recipes component

--- components/Recipes.tsx
+++ components/Recipes.tsx
@@ -1,46 +1,11 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
-import React, { useEffect, useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import { Link } from "expo-router";
 import Colors from "@/constants/Colors";
-
-interface Recipe {
-  id: string;
-  prep_time: string;
-  name: string;
-  about: string;
-  tags: string;
-  difficulty: string;
-  image: string;
-  cloudinary_id: string;
-  ingredients: string;
-  directions: string;
-  category_id: string;
-}
+import { useGetRecipesQuery } from "@/store/api";
 
 const Recipes = () => {
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-
-  const getRecipes = async () => {
-    try {
-      const response = await fetch("http://10.0.0.202:2828/api/recipes", {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      });
-      const json = await response.json();
-      setRecipes(json);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getRecipes();
-  }, []);
-
+  const { data, error, isLoading } = useGetRecipesQuery();
   return (
     <ScrollView
       horizontal
@@ -49,30 +14,31 @@ const Recipes = () => {
         padding: 15,
       }}
     >
-      {recipes.map((recipe, index) => (
-        <Link
-          href={{ pathname: "/details", params: { id: recipe.id } }}
-          key={index}
-          asChild
-        >
-          <TouchableOpacity>
-            <View style={styles.recipeCard}>
-              <Image source={{ uri: recipe.image }} style={styles.image} />
-              <View style={styles.recipeBox}>
-                <Text style={styles.recipeText}>{recipe.name}</Text>
-                <Text
-                  style={{ color: Colors.green, textTransform: "capitalize" }}
-                >
-                  {recipe.category_id} {recipe.prep_time}
-                </Text>
-                <Text style={{ color: Colors.medium }}>
-                  {recipe.difficulty}
-                </Text>
+      {data &&
+        data.map((recipe, index) => (
+          <Link
+            href={{ pathname: "/details", params: { id: recipe.id } }}
+            key={index}
+            asChild
+          >
+            <TouchableOpacity>
+              <View style={styles.recipeCard}>
+                <Image source={{ uri: recipe.image }} style={styles.image} />
+                <View style={styles.recipeBox}>
+                  <Text style={styles.recipeText}>{recipe.name}</Text>
+                  <Text
+                    style={{ color: Colors.green, textTransform: "capitalize" }}
+                  >
+                    {recipe.category_id} {recipe.prep_time}
+                  </Text>
+                  <Text style={{ color: Colors.medium }}>
+                    {recipe.difficulty}
+                  </Text>
+                </View>
               </View>
-            </View>
-          </TouchableOpacity>
-        </Link>
-      ))}
+            </TouchableOpacity>
+          </Link>
+        ))}
     </ScrollView>
   );
 };
