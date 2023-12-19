434e4fd80198274c306433e1809b9c0be65b4963
Moneer <moneer.alromimah@gmail.com>
2023-12-19 02:10:21 -0700

    add categories

---
+++ assets/data/categories.ts
@@ -0,0 +1,32 @@
+export const categories = [
+  {
+    id: "beef",
+    text: "Beef",
+    img: require("@/assets/data/beef.png"),
+  },
+  {
+    id: "chicken",
+    text: "Chicken",
+    img: require("@/assets/data/chicken.png"),
+  },
+  {
+    id: "halal",
+    text: "Halal",
+    img: require("@/assets/data/halal.png"),
+  },
+  {
+    id: "pork",
+    text: "Pork",
+    img: require("@/assets/data/pork.png"),
+  },
+  {
+    id: "seafood",
+    text: "Seafood",
+    img: require("@/assets/data/seafood.png"),
+  },
+  {
+    id: "vegeterian",
+    text: "Vegeterian",
+    img: require("@/assets/data/vegetarian.png"),
+  },
+];
