3a75ae2bbd22c5128d1a5b021d97bdaeb0bb8e77
Moneer <moneer.alromimah@gmail.com>
2023-12-19 02:15:10 -0700

    add index

---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import Categories from "@/components/Categories";
+import Colors from "@/constants/Colors";
+import Recipes from "@/components/Recipes";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Page = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+        <Categories />
+        <Text style={styles.header}>Trending today</Text>
+        <Recipes />
+        <Text style={styles.header}>Top picks for you</Text>
+        <Recipes />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    top: -20,
+    backgroundColor: Colors.lightGrey,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 16,
+    marginBottom: 8,
+    paddingHorizontal: 16,
+  },
+});
+
+export default Page;
