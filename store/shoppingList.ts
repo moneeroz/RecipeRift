fb6795d50d54bc7fe3db10dfc44802575a4562c7
Moneer <moneer.alromimah@gmail.com>
2023-12-30 14:45:25 -0700

    add shopping list slice

---
+++ store/shoppingList.ts
@@ -0,0 +1,64 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ImageSourcePropType } from "react-native";
+
+interface Ingredient {
+  id: string;
+  name: string;
+  quantity: number;
+  unit: string;
+  img: ImageSourcePropType;
+}
+
+interface ShoppingListState {
+  items: { [key: string]: { data: Ingredient } };
+}
+
+const initialState: ShoppingListState = {
+  items: {},
+};
+
+const shoppingListSlice = createSlice({
+  name: "shoppingList",
+  initialState,
+  reducers: {
+    // && state.items[id].data.unit === action.payload.unit
+    addIngredientToShoppingList: (state, action: PayloadAction<Ingredient>) => {
+      const id = action.payload.id;
+      if (
+        state.items[id] &&
+        state.items[id].data.unit === action.payload.unit
+      ) {
+        state.items[id].data.quantity += action.payload.quantity;
+      } else {
+        state.items[id] = { data: action.payload };
+      }
+    },
+    removeIngredientFromShoppingList: (
+      state,
+      action: PayloadAction<Ingredient>,
+    ) => {
+      const id = action.payload.id;
+      if (
+        state.items[id] &&
+        state.items[id].data.unit === action.payload.unit &&
+        state.items[id].data.quantity > 0
+      ) {
+        state.items[id].data.quantity -= action.payload.quantity;
+      }
+      if (state.items[id].data.quantity === 0) {
+        delete state.items[id];
+      }
+    },
+    clearShoppingList: (state) => {
+      state.items = {};
+    },
+  },
+});
+
+export const {
+  addIngredientToShoppingList,
+  removeIngredientFromShoppingList,
+  clearShoppingList,
+} = shoppingListSlice.actions;
+
+export default shoppingListSlice.reducer;
--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api";
 import basketReducer from "./basket";
+import shoppingListReducer from "./shoppingList";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     basket: basketReducer,
+    shoppingList: shoppingListReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
