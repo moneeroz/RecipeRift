9ee45d2d52533712f30af753a2df95b76da5799d
Moneer <moneer.alromimah@gmail.com>
2024-01-02 19:10:25 -0700

    implement search functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,47 @@
 import { Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import Categories from "@/components/Categories";
 import Colors from "@/constants/Colors";
 import Recipes from "@/components/Recipes";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Stack } from "expo-router";
+import SearchBar from "@/components/SearchBar";
+import { useGetRecipesQuery } from "@/store/api";
+import RecipeList from "@/components/RecipeList";
 
 const Page = () => {
+  const { data, error, isLoading } = useGetRecipesQuery();
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredRecipes = data?.filter((recipe) =>
+    recipe.name.includes(searchTerm),
+  );
+
+  const handleSearchTermChange = (newSearchTerm: string) => {
+    setSearchTerm(newSearchTerm);
+  };
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
-        <Categories />
-        <Text style={styles.header}>Trending today</Text>
-        <Recipes />
-        <Text style={styles.header}>Top picks for you</Text>
-        <Recipes />
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <Stack.Screen
+        options={{
+          header: () => (
+            <SearchBar onSearchTermChange={handleSearchTermChange} />
+          ),
+        }}
+      />
+      <SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+          <Categories />
+          {searchTerm ? (
+            <RecipeList height={50} recipes={filteredRecipes} />
+          ) : null}
+          <Text style={styles.header}>Trending today</Text>
+          <Recipes data={data} />
+          <Text style={styles.header}>Top picks for you</Text>
+          <Recipes data={data} />
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
